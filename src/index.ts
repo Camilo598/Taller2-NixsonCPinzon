9ca9dba0cc3b128caef2302ad51d94e2428cf6fe
Nixson Camilo Pinzon <pinzon0930@gmail.com>
2023-04-26 12:19:40 -0500

    Ajuste del index.html y crear evento click del informe

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,14 @@ btnGuardar.addEventListener('click', (e) =>{
 
     //La validacion del formulario
 
-    const tipoDocumento = document.getElementById('tipoDcoumento') as HTMLInputElement;
-    const numeroDocumento = document.getElementById('numeroDocumento') as HTMLInputElement;
-    const nombre = document.getElementById('nombre') as HTMLInputElement;
-    const apellido = document.getElementById('apellido') as HTMLInputElement;
+    const tipoIdentificacion = document.getElementById('tipoIdentificacion') as HTMLInputElement;
+    const numeroIdentificacion = document.getElementById('numeroIdentificacion') as HTMLInputElement;
+    const nombres = document.getElementById('nombres') as HTMLInputElement;
+    const apellidos = document.getElementById('apellidos') as HTMLInputElement;
     const celular = document.getElementById('celular') as HTMLInputElement;
     const email = document.getElementById('email') as HTMLInputElement;
     const linkedin = document.getElementById('linkedin') as HTMLInputElement;
     const github = document.getElementById('github') as HTMLInputElement;
 
-    console.log(tipoDocumento.value);
+    console.log(tipoIdentificacion.value);
 })
